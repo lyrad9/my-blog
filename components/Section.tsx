f299ef1a855fe179a4348632bb535b620daf7ebb
Daryl <borismbakop611@gmail.com>
2024-09-29 04:43:48 +0100

    Update Section.tsx

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react'
 import { cn } from '@/lib/utils'
 export const Section = (props:PropsWithChildren<{className?:string}>) => {
   return (
-   <section className={cn('w-full max-w-4xl px-4  m-auto',props.className)}>
+   <section className={cn('w-full max-w-4xl px-4  mx-auto',props.className)}>
     {props.children}
    </section>
   )
