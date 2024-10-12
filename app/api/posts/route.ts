0212294f65c0d6971e7341ae2c7340bbfe75bcc9
Daryl <borismbakop611@gmail.com>
2024-10-12 01:44:47 +0100

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -4,10 +4,17 @@ import { NextResponse,NextRequest } from 'next/server';
 export async function GET(req: NextRequest) {
   //  if(!req ) return  NextResponse.error()
     // const body= await req.json()
-  const recentPosts = [...posts].slice(0,3)
-//     const filteredPosts = posts.filter((post) => post.lang === body.locale)
-//        const sortedPosts = sortPosts(filteredPosts)
-//   const recentPosts = sortedPosts.slice(0,3)
+  const {searchParams} = new URL(req.url)
+ const locale = searchParams.get("locale")
+
+    // Vérifier si la langue est spécifiée
+    if (!locale) {
+        return NextResponse.json({ error: 'Locale not specified' }, { status: 400 });
+      }
+  
+    const filteredPosts = posts.filter((post) => post.lang === locale)
+       const sortedPosts = sortPosts(filteredPosts)
+  const recentPosts = sortedPosts.slice(0,3)
   console.log(JSON.stringify(recentPosts))
     return NextResponse.json({ posts: recentPosts });
   }
