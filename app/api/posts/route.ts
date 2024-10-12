12aa5c02732d011855bdef18e4c0c80fbeba1cd0
Daryl <borismbakop611@gmail.com>
2024-10-12 02:40:42 +0100

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,29 +1,41 @@
-import {posts} from "#site/content"
+import { posts } from "#site/content";
 import { sortPosts } from "@/src/components/utils/functions";
-import { NextResponse,NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
-// res.headers.set("Access-Control-Allow-Origin","http://localhost:3001")
-  const {searchParams} = new URL(req.url)
- const locale = searchParams.get("locale")
+  // Liste blanche des origines autorisées
+  const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+  // Récupérer l'origine de la requête
+  const origin = req.headers.get("origin");
 
-    // Vérifier si la langue est spécifiée
-    if (!locale) {
-        return new NextResponse('Locale not specified', { status: 400 });
-        // return NextResponse.json({ error: 'Locale not specified' }, { status: 400 });
-      }
-  
-    const filteredPosts = posts.filter((post) => post.lang === locale)
-       const sortedPosts = sortPosts(filteredPosts)
-  const recentPosts = sortedPosts.slice(0,3)
-  console.log(JSON.stringify(recentPosts))
-  const response = NextResponse.json({ posts: recentPosts });
-    
-    // Ajouter les en-têtes CORS
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  // Vérifier si l'origine est dans la liste blanche
+  const isAllowedOrigin = allowedOrigins.includes(origin || "");
 
-    return response;
-   // return NextResponse.json({ posts: recentPosts });
+  if (!isAllowedOrigin) {
+    // Si l'origine n'est pas autorisée, renvoyer une réponse 403 (Interdit)
+    return new NextResponse("Forbidden", { status: 403 });
   }
-  
+
+  const { searchParams } = new URL(req.url);
+  const locale = searchParams.get("locale");
+
+  // Vérifier si la langue est spécifiée
+  if (!locale) {
+    return new NextResponse("Locale not specified", { status: 400 });
+    // return NextResponse.json({ error: 'Locale not specified' }, { status: 400 });
+  }
+
+  const filteredPosts = posts.filter((post) => post.lang === locale);
+  const sortedPosts = sortPosts(filteredPosts);
+  const recentPosts = sortedPosts.slice(0, 3);
+  console.log(JSON.stringify(recentPosts));
+  const response = NextResponse.json({ posts: recentPosts });
+
+  // Ajouter les en-têtes CORS
+  if (isAllowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", origin as string);
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  }
+  return response;
+  // return NextResponse.json({ posts: recentPosts });
+}
