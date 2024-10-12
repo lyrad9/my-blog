aeb4acf71d7ca8d9bb2b00cc4cb355bffd6e67b3
Daryl <borismbakop611@gmail.com>
2024-10-12 01:25:26 +0100

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,12 +1,13 @@
 import {posts} from "#site/content"
 import { sortPosts } from "@/src/components/utils/functions";
 import { NextResponse,NextRequest } from 'next/server';
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
   //  if(!req ) return  NextResponse.error()
-    const body= await req.json()
-    const filteredPosts = posts.filter((post) => post.lang === body.locale)
-       const sortedPosts = sortPosts(filteredPosts)
-  const recentPosts = sortedPosts.slice(0,3)
+    // const body= await req.json()
+  const recentPosts = [...posts].slice(0,3)
+//     const filteredPosts = posts.filter((post) => post.lang === body.locale)
+//        const sortedPosts = sortPosts(filteredPosts)
+//   const recentPosts = sortedPosts.slice(0,3)
   console.log(JSON.stringify(recentPosts))
     return NextResponse.json({ posts: recentPosts });
   }
