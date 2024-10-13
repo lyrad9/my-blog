eba087f92e4e3393c89ab9a104413b5f6b86e9e1
Daryl <borismbakop611@gmail.com>
2024-10-13 16:09:59 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,27 +7,26 @@ const I18nMiddleware = createI18nMiddleware({
   defaultLocale: 'fr',
 
 })
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "https://darylblog.vercel.app",
- "https://radylb.vercel.app"
-];
+ 
 export function middleware(request: NextRequest) {
   //return I18nMiddleware(request)
    // Apply the i18n middleware first
    const i18nResponse = I18nMiddleware(request);
-  //  i18nResponse.headers.set('request-ip', request.ip || 'unknown')
-  // const origin = request.headers.get("origin")
-  // if (allowedOrigins.includes(origin as string)) {
-  //   i18nResponse.headers.append("Access-Control-Allow-Origin", origin as string)
-  // }
-  // i18nResponse.headers.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  // i18nResponse.headers.append("Access-Control-Allow-Headers", "Content-Type");
-   return i18nResponse                                                                                                                                     
+   i18nResponse.headers.set('request-ip', request.ip || 'unknown')
+   return i18nResponse
+  // Modify request headers
+  // const requestHeaders = new Headers(request.headers);
+  // requestHeaders.set('request-ip', request.ip || 'unknown');
+  //   // Return the modified response with updated headers
+  //   return NextResponse.next({
 
+  //     request: {
+  //       headers: requestHeaders,
+        
+  //     },
+  //   });
 }
  
 export const config = {
-  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)',"/api/:path*"]
+  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)']
 }
