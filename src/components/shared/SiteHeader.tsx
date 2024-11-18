63fbd84aa40654cdbf8fcc3200b21a145ef08834
Daryl <borismbakop611@gmail.com>
2024-11-18 02:42:31 +0100

    Update SiteHeader.tsx

--- src/components/shared/SiteHeader.tsx
+++ src/components/shared/SiteHeader.tsx
@@ -40,7 +40,14 @@ export const SiteHeader = () => {
 
              
             </nav> */}
-
+{/*  <SunIcon
+        size={40}
+        className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      />
+      <MoonIcon
+        size={20}
+        className="absolute rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100 "
+      /> */}
           <ThemeToggle />
         </div>
       </Section>
